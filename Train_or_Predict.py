a3af3d13d8afb3c0b48b50a1b35864f47313500a
Bishnu Poudel <replytobishnu@gmail.com>
2021-05-12 16:44:10 +0545

    plotting a single image works. Next step is video and image side by side.

--- Train_or_Predict.py
+++ Train_or_Predict.py
@@ -3,16 +3,30 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import array_to_img
 from keras.models import load_model
 from Upload_Picture import take_picture
 from PIL import Image
-
 
 # Using the model we trained using the hair dataset in Unet
 class Train_or_Predict():
 
     def __init__(self, image_path='.'):
         self.image_path = image_path
+
+    def get_mask_from_picture(self):
+        #lod the pretrained model
+        model_up = load_model('93_Percent_HairSegmentation.h5')
+        # take picture function
+        input_image, image_size = take_picture('my_picture')
+        # resize input image
+        arr = self.resize_input_to_model_size(input_image)
+        #predict with model
+        output = model_up.predict(arr)
+        print('output', output)
+        # get output image same size as input
+        output_mask = self.resize_model_to_input_size(output, image_size)
+        return output_mask
 
     def resize_input_to_model_size(self, input_image):
         img = load_img(input_image, target_size=(256, 256))
@@ -21,26 +35,10 @@ class Train_or_Predict():
         return arr
 
     def resize_model_to_input_size(self, model_output_image, image_size):
-        mask = np.squeeze(model_output_image, axis=(0, 3))
-        mask_img = Image.fromarray(mask).resize(image_size)
+        mask = np.squeeze(model_output_image, axis=(0))
+        mask_img = array_to_img(mask).resize(image_size)
+        # mask_img = Image.fromarray(mask).resize(image_size)
         return mask_img
-
-    def get_mask_from_picture(self):
-        model_up = load_model('93_Percent_HairSegmentation.h5')
-        # take picture function
-        input_image, image_size = take_picture('my_picture')
-        # resize input image
-        arr = self.resize_input_to_model_size(input_image)
-
-        #predict with model
-        output = model_up.predict(arr)
-
-        # mask = np.squeeze(output, axis=(0,3))
-        # Convert to PIL Image and save
-        # return Image.fromarray(mask)
-        # new method
-        output_mask = self.resize_model_to_input_size(output, image_size)
-        return output_mask
 
     def plot_sample_pictures(self):
         pass
@@ -52,4 +50,7 @@ class Train_or_Predict():
 if __name__ == "__main__":
     tp = Train_or_Predict()
     image = tp.get_mask_from_picture()
-    image.save('mask.tif')
+    image.save('mask.tiff')
+    plt.imshow(image,cmap='gray')
+    plt.show()
+
