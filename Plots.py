9a64bc45df244246f0c5427b899b80c0ce32a8d0
Bishnu Poudel <replytobishnu@gmail.com>
2021-05-15 20:40:24 +0545

    Overlapping 2 images works

--- Plots.py
+++ Plots.py
@@ -4,6 +4,8 @@ from Train_or_Predict import Train_or_P
 from Upload_Picture import take_picture_frames_in_video
 import numpy as np
 import cv2
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import array_to_img
 
 class ContinuousPlots():
 
@@ -16,7 +18,12 @@ class ContinuousPlots():
         img_gen = take_picture_frames_in_video(tp.image_name)
         for array, cam in img_gen:
             mask = tp.get_mask_from_array(array)
-            ax.imshow(mask, cmap='gray')
+            mask = np.array(mask)
+            # Otsu's thresholding
+            th , maskt = cv2.threshold(mask, 0.0, 1.0, cv2.THRESH_BINARY_INV+ cv2.THRESH_OTSU)
+            maskt = np.expand_dims(maskt, axis=2)
+            ax.imshow( np.multiply( array , maskt ) )
+            # ax.imshow(maskt, cmap='gray')
             plt.pause(1 / 4)
             ax.cla()
 
